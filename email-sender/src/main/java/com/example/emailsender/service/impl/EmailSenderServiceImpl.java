f6bd190ab3f55ee5326814de2a5456b3b2c20ab5
Gockster <gm.dimitriadis@hotmail.com>
2023-04-22 23:51:43 +0300

    the commit

---
+++ email-sender/src/main/java/com/example/emailsender/service/impl/EmailSenderServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.emailsender.service.impl;
+
+import com.example.emailsender.service.EmailSenderService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderServiceImpl implements EmailSenderService {
+    private JavaMailSender mailSender;
+
+    public EmailSenderServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String message) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("gmd.dimitriadis@gmail.com");
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+
+        this.mailSender.send(simpleMailMessage);
+
+    }
+}
